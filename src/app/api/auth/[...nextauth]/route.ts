633c0d15a4e4b7d7649f5aa4463d527a83da76e2
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2025-06-29 19:05:23 +0200

    feat: google auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,14 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { getUserByEmail } from "@/lib/users";
 import type { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SessionClientProvider from "@/context/SessionClientProvider";
+import SessionClientProvider from "@/providers/SessionClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

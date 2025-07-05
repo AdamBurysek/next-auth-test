cea903ad8c0def2e2fc3769463a980fe0a98ef32
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2025-07-05 16:23:09 +0200

    feat: pool change

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,9 +2,7 @@ import { Pool } from "pg";
 
 // Database connection
 export const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST || "localhost",
-  port: Number(process.env.DATABASE_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
+
